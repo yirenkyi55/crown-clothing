0736f6b87b9f8cf26161428cf892edc1ae7f8e26
Emmanuel Yirenkyi <fb_emmanuel@outlook.com>
2021-01-15 04:49:51 +0000

    Implemented firebase utils, included ability to store authenticated users into firebase database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -20,9 +20,26 @@ class App extends React.Component {
   unSubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unSubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
+        });
+      } else {
+        this.setState({ currentUser: null });
+      }
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,18 +3,45 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyBWX_JKy2NY-55Gzd5gPhSJdRoT1topRko',
-  authDomain: 'crwn-db-4e914.firebaseapp.com',
-  projectId: 'crwn-db-4e914',
-  storageBucket: 'crwn-db-4e914.appspot.com',
-  messagingSenderId: '894158633460',
-  appId: '1:894158633460:web:8188d1aacee500f496e1a1',
+  apiKey: 'AIzaSyA4IM1NWLU9mgQ8dSSSu9uHRTSWHvy4Ykk',
+  authDomain: 'crown-db-b3078.firebaseapp.com',
+  projectId: 'crown-db-b3078',
+  storageBucket: 'crown-db-b3078.appspot.com',
+  messagingSenderId: '477963791317',
+  appId: '1:477963791317:web:bd349b032cb2a5220d6b5c',
 };
 
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+
+// When a user sign in with google, we save the data into firestore database
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  // We query inside firestore to see if the user already exists
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
