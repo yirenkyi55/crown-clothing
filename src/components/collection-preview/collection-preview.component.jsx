7b56ddae67ae07e426217509b0739681d1ad62d2
Emmanuel Yirenkyi <fb_emmanuel@outlook.com>
2021-01-19 06:48:09 +0000

    added cart utils to group item in cart reducer, updated shop-data.js

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className='preview'>
         {items
           .filter((_, idx) => idx < 4)
-          .map(({ id, ...props }) => (
-            <CollectionItem key={id} id={id} {...props} />
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
           ))}
       </div>
     </div>
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,15 +1,22 @@
 import { CartActionTypes } from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
-const cartReducer = (state = INITIAL_STATE, { type }) => {
+const cartReducer = (state = INITIAL_STATE, { type, payload }) => {
   switch (type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
         hidden: !state.hidden,
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, payload),
       };
     default:
       return state;
